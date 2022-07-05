77bdd09fb21992efd19538c7d02fa3a8aea737df
evertonleonel <everton_leonel@hotmail.com>
2022-07-04 21:46:53 -0300

    Adicionado ocultação de senha

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,50 @@
-function habilitaBotao(){
-    let nome_input = document.querySelector('.nome_input').value;
-    let senha_input = document.querySelector('.senha_input').value;
+function habilitaBotao() {
+  let nome_input = document.querySelector('.nome_input').value;
+  let senha_input = document.querySelector('.senha_input').value;
 
-    if (nome_input && senha_input){
-        let button = document.querySelector('.login_button').disabled = false
-        return;
-    }
-        document.querySelector('.login_button').disabled = true
+  if (nome_input && senha_input) {
+    let button = document.querySelector('.login_button').disabled = false
+    return;
+  }
+  document.querySelector('.login_button').disabled = true
 }
 
-
-function abrirLink (){
-    window.location.href = "/src/html/banco.html";
+function abrirLink() {
+  window.location.href = "/src/html/banco.html";
 }
+
+// Mudar a senha
+
+let password = document.getElementById('password')
+let olho = document.querySelector('.olho');
+let imgOlho = document.querySelector('.olho img')
+
+
+olho.addEventListener('click', function () {
+
+  let tipoPassword = password.type === 'password'
+
+  if (tipoPassword) {
+    mostrarPassword();
+    imgMostraSenha();
+  } else {
+    escondePassord();
+    imgEscondeSenha();
+  };
+
+  function mostrarPassword() {
+    password.setAttribute('type', 'text');
+  }
+
+  function escondePassord() {
+    password.setAttribute('type', 'password');
+  }
+
+  function imgMostraSenha() {
+    imgOlho.setAttribute('src', '/src/imagens/eye.png');
+  }
+
+  function imgEscondeSenha() {
+    imgOlho.setAttribute('src', '/src/imagens/close.png');
+  }
+});
